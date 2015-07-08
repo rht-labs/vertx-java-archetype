e39e430eaaf549e455dd4f2060c1be425b47bb68
Deven Phillips <deven.phillips@gmail.com>
2015-07-08 12:59:29 -0400

    Storing changes for later

--- src/main/resources/archetype-resources/src/main/java/Main.java
+++ src/main/resources/archetype-resources/src/main/java/Main.java
@@ -1,19 +1,14 @@
-#set( $symbol_pound = '#' )                                                                                                                                                                                                              
-#set( $symbol_dollar = '$' )                                                                                                                                                                                                             
-#set( $symbol_escape = '\' )                                                                                                                                                                                                             
 package ${package};
 
-import io.vertx.core.AbstractVerticle;
-
 /**
+ * @author <a href="mailto: ${emailAddress}">${givenName} ${familyName}</a>
  *
  */
 public class Main extends AbstractVerticle {
 
-  @Override
-  public void start() throws Exception {
-    vertx.setPeriodic(3000, res -> {
-      System.out.println("Periodic event triggered.");
-    });
-  }
+    @Override
+    public void start() {
+        // 
+    }
+
 }
